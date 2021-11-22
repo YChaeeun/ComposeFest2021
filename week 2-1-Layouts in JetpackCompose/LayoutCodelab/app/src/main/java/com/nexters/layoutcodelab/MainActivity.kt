dd0fc7720039684b7f26c5b806ababf8d684769f
YChaeeun <ychae.leah@gmail.com>
2021-11-22 22:30:38 +0900

    layouts - ~10. Constraint Layout

--- week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/nexters/layoutcodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/nexters/layoutcodelab/MainActivity.kt
@@ -27,6 +27,10 @@ import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.atLeast
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.nexters.layoutcodelab.ui.theme.LayoutCodelabTheme
@@ -43,6 +47,111 @@ class MainActivity : ComponentActivity() {
 //			LayoutCodelabs()
 //			PhotographerCard()
 		}
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConstraintLayoutContent_Preview() {
+	LayoutCodelabTheme {
+		DecoupledConstraintLayout()
+//		LargeConstraintLayout()
+//		ConstraintLayoutContent()
+	}
+}
+
+@Composable
+fun DecoupledConstraintLayout() {
+	BoxWithConstraints {
+		val constraints = if (maxWidth < maxHeight) {
+			decoupledConstraints(margin = 16.dp) // portrait
+		} else {
+			decoupledConstraints(margin = 32.dp) // landscape
+		}
+
+		// pass ConstraintSet parameter & assign references using layoutId
+		ConstraintLayout(constraints) {
+			Button(
+				onClick = {},
+				modifier = Modifier.layoutId("button")
+			) {
+				Text("Button")
+			}
+
+			Text("text", Modifier.layoutId("text"))
+		}
+	}
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+	return ConstraintSet {
+		val button = createRefFor("button")
+		val text = createRefFor("text")
+
+		constrain(button) {
+			top.linkTo(parent.top, margin)
+		}
+
+		constrain(text) {
+			top.linkTo(button.bottom, margin)
+		}
+	}
+}
+
+@Composable
+fun LargeConstraintLayout() {
+	ConstraintLayout {
+		val text = createRef()
+
+		val guideline = createGuidelineFromStart(fraction = 0.5f)
+		Text(
+			"This is a very very very very very very very very very long text",
+			Modifier.constrainAs(text) {
+				linkTo(start = guideline, end = parent.end)
+				width = Dimension.preferredWrapContent.atLeast(100.dp)
+				/** Dimension
+				 *  - preferredWrapContent : wrap content, subject to the constraints in that dimension
+				 *  - wrapContent : wrap content, even if the constraints would not allow it
+				 *  - fillToConstraints : expand to fill the space defined by its constraints in that dimension
+				 *  - preferredValue : fixed dp value, subject to the constraints in that dimension
+				 *  - value : fixed dp value, regardless of constraints in that dimension
+				 */
+			}
+		)
+	}
+}
+
+@Composable
+fun ConstraintLayoutContent() {
+	ConstraintLayout {
+		// Create reference for the composable to ConstraintLayout's body
+		val (button1, button2, text) = createRefs()
+
+		Button(
+			onClick = {},
+			modifier = Modifier.constrainAs(button1) {
+				top.linkTo(parent.top, margin = 16.dp)
+			}
+		) {
+			Text("Button 1")
+		}
+
+		// barriers (and all the other helpers) can be created in the body of ConstraintLayout, not inside constrainAs
+		val barrier = createEndBarrier(button1, text)
+		Button(
+			onClick = {},
+			modifier = Modifier.constrainAs(button2) {
+				top.linkTo(parent.top, margin = 16.dp)
+				start.linkTo(barrier)
+			}
+		) {
+			Text("Button 2")
+		}
+
+		Text("Text", Modifier.constrainAs(text) {
+			top.linkTo(button1.bottom, margin = 16.dp)
+			centerAround(button1.end) // linkTo(anchor, anchor)
+		})
 	}
 }
 
