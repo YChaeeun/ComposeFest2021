f736847eb267763b7e1b442be3f80791f454e2aa
YChaeeun <ychae.leah@gmail.com>
2021-12-10 08:51:30 +0900

    navigation ~6. Extract finished NavHost

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -23,14 +23,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.navigation.NavType
+import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import androidx.navigation.navDeepLink
 import com.example.compose.rally.data.UserData
 import com.example.compose.rally.ui.accounts.AccountsBody
 import com.example.compose.rally.ui.accounts.SingleAccountBody
@@ -70,52 +67,60 @@ fun RallyApp() {
 				)
 			}
 		) { innerPadding ->
-			NavHost(
-				navController = navController,
-				startDestination = RallyScreen.Overview.name,
-				modifier = Modifier.padding(innerPadding)
-			) {
-				composable(RallyScreen.Overview.name) {
-					OverviewBody(
-						onClickSeeAllAccounts = { navController.navigate(RallyScreen.Accounts.name) },
-						onClickSeeAllBills = { navController.navigate(RallyScreen.Bills.name) },
-						onAccountClick = { name -> navigateToSingleAccount(navController, name) }
-					)
-				}
-				composable(RallyScreen.Accounts.name) {
-					AccountsBody(accounts = UserData.accounts) { name ->
-						navigateToSingleAccount(
-							navController = navController,
-							accountName = name
-						)
-					}
-				}
-				composable(RallyScreen.Bills.name) {
-					BillsBody(bills = UserData.bills)
-				}
+			RallyNavHost(navController = navController, modifier = Modifier.padding(innerPadding))
+		}
+	}
+}
 
-				val accountsName = RallyScreen.Accounts.name
-				composable(
-					route = "$accountsName/{name}",
-					arguments = listOf(
-						navArgument("name") {
-							// Make argument type safe
-							type = NavType.StringType
-						}
-					),
-					deepLinks = listOf(navDeepLink {
-						uriPattern = "rally://$accountsName/{name}"
-					})
-					/**
-					 * Test the deep link using adb
-					 *  > adb shell am start -d "rally://accounts/Checking" -a android.intent.action.VIEW
-					 */
-				) { entry -> // Look up "name" in NavBackstackEntry's arguments
-					val accountName = entry.arguments?.getString("name")
-					val account = UserData.getAccount(accountName)
-					SingleAccountBody(account = account)
-				}
+@Composable
+fun RallyNavHost(
+	navController: NavHostController,
+	modifier: Modifier =  Modifier
+) {
+	NavHost(
+		navController = navController,
+		startDestination = RallyScreen.Overview.name,
+		modifier = modifier
+	) {
+		composable(RallyScreen.Overview.name) {
+			OverviewBody(
+				onClickSeeAllAccounts = { navController.navigate(RallyScreen.Accounts.name) },
+				onClickSeeAllBills = { navController.navigate(RallyScreen.Bills.name) },
+				onAccountClick = { name -> navigateToSingleAccount(navController, name) }
+			)
+		}
+		composable(RallyScreen.Accounts.name) {
+			AccountsBody(accounts = UserData.accounts) { name ->
+				navigateToSingleAccount(
+					navController = navController,
+					accountName = name
+				)
 			}
+		}
+		composable(RallyScreen.Bills.name) {
+			BillsBody(bills = UserData.bills)
+		}
+
+		val accountsName = RallyScreen.Accounts.name
+		composable(
+			route = "$accountsName/{name}",
+			arguments = listOf(
+				navArgument("name") {
+					// Make argument type safe
+					type = NavType.StringType
+				}
+			),
+			deepLinks = listOf(navDeepLink {
+				uriPattern = "rally://$accountsName/{name}"
+			})
+			/**
+			 * Test the deep link using adb
+			 *  > adb shell am start -d "rally://accounts/Checking" -a android.intent.action.VIEW
+			 */
+		) { entry -> // Look up "name" in NavBackstackEntry's arguments
+			val accountName = entry.arguments?.getString("name")
+			val account = UserData.getAccount(accountName)
+			SingleAccountBody(account = account)
 		}
 	}
 }
