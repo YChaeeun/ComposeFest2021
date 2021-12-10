9bc8f77df284767480405fed2b8f14ab22a36e31
YChaeeun <ychae.leah@gmail.com>
2021-12-10 17:48:18 +0900

    migration ~9. HTML - Views in Compose Code

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,15 +16,19 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -32,10 +36,39 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
+
+@Composable
+private fun PlantDescription(description: String) {
+	// Remembers the HTML formatted description. Re-executes on a new description
+	val htmlDescription = remember(description) {
+		HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+	}
+
+	AndroidView(
+		factory = { context ->
+			TextView(context).apply {
+				movementMethod = LinkMovementMethod.getInstance()
+			}
+		},
+		update = {
+			it.text = htmlDescription
+		}
+	)
+}
+
+@Preview
+@Composable
+private fun PlantDescriptionPreview() {
+	MaterialTheme {
+		PlantDescription(description = "HTML<br><br>description")
+	}
+}
 
 @Composable
 fun PlantDetailDescription(viewModel: PlantDetailViewModel) {
@@ -48,13 +81,19 @@ fun PlantDetailDescription(viewModel: PlantDetailViewModel) {
 
 @Composable
 fun PlantDetailContent(plant: Plant) {
-	PlantName(plant.name)
+	Surface {
+		Column(Modifier.padding(dimensionResource(R.dimen.margin_normal))) {
+			PlantName(plant.name)
+			PlantWatering(plant.wateringInterval)
+			PlantDescription(plant.description)
+		}
+	}
 }
 
 @Preview
 @Composable
 private fun PlantDetailContentPreview() {
-	val plant = Plant("id", "Grape", "description", 3, 30, "")
+	val plant = Plant("id", "Grape", "HTML<br><br>description", 3, 30, "")
 	MaterialTheme {
 		PlantDetailContent(plant = plant)
 	}
